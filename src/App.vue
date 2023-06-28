b61b1257b7e3e0a70b6f5b27729448eb132e378c
matteo2302 <matteo.pizzichemi01@gmail.com>
2023-06-28 16:32:56 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import Apptitle from './components/Apptitle.vue'
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    Apptitle: Apptitle,
+  }
+}
+</script>
+
+<template>
+  <Apptile />
+</template>
+
+<style ></style>
---
+++ src/components/Apptitle.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>La mia prima app con Vite!</h1>
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
